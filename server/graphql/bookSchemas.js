88a3e3c0cc5924561f8daeef74468f4384cc9b4c
Matthew J. Whitney <mwhitney@agoraaltx.com>
2019-10-20 12:30:57 -0700

    Create GraphQL Schemas for Book

---
+++ server/graphql/bookSchemas.js
@@ -0,0 +1,78 @@
+var GraphQLSchema = require('graphql').GraphQLSchema;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLList = require('graphql').GraphQLList;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLNonNull = require('graphql').GraphQLNonNull;
+var GraphQLID = require('graphql').GraphQLID;
+var GraphQLString = require('graphql').GraphQLString;
+var GraphQLInt = require('graphql').GraphQLInt;
+var GraphQLDate = require('graphql-date');
+var BookModel = require('../models/Book');
+
+var bookType = new GraphQLObjectType({
+    name: 'book',
+    fields: function () {
+        return {
+            _id: {
+                type: GraphQLString
+            },
+            isbn: {
+                type: GraphQLString
+            },
+            title: {
+                type: GraphQLString
+            },
+            author: {
+                type: GraphQLString
+            },
+            description: {
+                type: GraphQLString
+            },
+            published_year: {
+                type: GraphQLInt
+            },
+            publisher: {
+                type: GraphQLString
+            },
+            updated_date: {
+                type: GraphQLDate
+            }
+        }
+    }
+});
+
+var queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: function () {
+        return {
+            books: {
+                type: new GraphQLList(bookType),
+                resolve: function () {
+                    const books = BookModel.find().exec()
+                    if (!books) {
+                        throw new Error('Error')
+                    }
+                    return books
+                }
+            },
+            book: {
+                type: bookType,
+                args: {
+                    id: {
+                        name: '_id',
+                        type: GraphQLString
+                    }
+                },
+                resolve: function (root, params) {
+                    const bookDetails = BookModel.findById(params.id).exec()
+                    if (!bookDetails) {
+                        throw new Error('Error')
+                    }
+                    return bookDetails
+                }
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({ query: queryType });
