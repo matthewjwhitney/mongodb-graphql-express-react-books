614fa7bb9c3939875450ffce0f0579f466a6ddbf
Matthew J. Whitney <mwhitney@agoraaltx.com>
2019-10-20 12:57:22 -0700

    Create React.js Application and install react apollo

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient();
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
